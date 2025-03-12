3a6b404b01c27afff1d3d73cd502107f619118f4
TranMinhNhut <you@example.com>
2025-03-12 10:29:04 +0700

    Thêm Form2 vào project

---
+++ Tran_Minh_Nhut_2280602263/Form2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tran_Minh_Nhut_2280602263
+{
+    public partial class Form2: Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+    }
+}
